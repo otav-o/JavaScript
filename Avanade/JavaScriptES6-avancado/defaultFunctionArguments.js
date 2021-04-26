a885364a0e9d4274e7b4426b604d9c5514d0d688
otav-o <otaviocacadordioscanio@gmail.com>
2021-04-26 15:19:14 -0300

    Parâmetros padrão de funções

---
+++ Avanade/JavaScriptES6-avancado/defaultFunctionArguments.js
@@ -0,0 +1,31 @@
+// se não passar um parâmetro, ele fica undefined. Multiplicar um número por undefined, retorna NaN (not a number)
+
+function multiply(a, b) {
+    // b = b || 1; problema se b for 0
+
+    // b = typeof b === 'undefined' ? 1 : b; resolve mas fica verboso
+    return a * b;
+}
+
+console.log(multiply(5, 0));
+
+function multiply2(a, b = 1) {
+    return a * b;
+}
+
+console.log(multiply2(4, undefined)); // 4
+console.log(multiply2(undefined, 4)); // 4
+
+
+// Lazy evaluation: função só é invocada se o parâmetro estiver faltando
+
+function multiply3(a, b = randomNumber()) {
+    return a * b;
+}
+console.log(multiply3(5));
+console.log(multiply3(5, 10));
+
+function randomNumber() {
+    console.log('Número aleatório gerado');
+    return Math.random() * 10;
+}
